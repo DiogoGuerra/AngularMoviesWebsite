0a5f95eb9febac2babdb23fcf93c8cfe9d96537a
Diogo Guerra <diogoguerra25@gmail.com>
2023-01-04 17:24:54 +0000

    Created service to get info from api + Created banner with carousel based on https://getbootstrap.com/docs/5.3/components/carousel/

--- showtime/src/app/pages/home/home.component.ts
+++ showtime/src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
 
+  bannerResult: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+
+  // bannerdata
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      console.log(result, 'bannerresult#');
+      this.bannerResult = result.results;
+    });
+  }
 }

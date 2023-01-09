02b3985ab77d4ec4877dbaa22b476777513b10e8
Diogo Guerra <diogoguerra25@gmail.com>
2023-01-09 16:25:09 +0000

    Created Details Page + Trailer Button + Top Cast

--- showtime/src/app/pages/movie-details/movie-details.component.ts
+++ showtime/src/app/pages/movie-details/movie-details.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
 })
-export class MovieDetailsComponent {
+export class MovieDetailsComponent implements OnInit {
+  constructor(
+    private service: MovieApiServiceService,
+    private router: ActivatedRoute
+  ) {}
 
+  //Defining variables
+  getMovieDetailResult: any;
+  getMovieVideoResult: any;
+  getMovieCastResult: any;
+
+  ngOnInit(): void {
+    let getParamId = this.router.snapshot.paramMap.get('id');
+    console.log(getParamId, 'getparamid#');
+    this.getMovies(getParamId);
+    this.getVideo(getParamId);
+    this.getMovieCast(getParamId);
+  }
+
+  getMovies(id: any) {
+    this.service.getMovieDetails(id).subscribe((result) => {
+      console.log(result, 'getmoviedetails#');
+      this.getMovieDetailResult = result;
+    });
+  }
+
+  getVideo(id: any) {
+    this.service.getMovieVideo(id).subscribe((result) => {
+      console.log(result, 'getMovieVideo#');
+      result.results.forEach((element: any) => {
+        if (element.type == 'Trailer') {
+          this.getMovieVideoResult = element.key;
+        }
+      });
+    });
+  }
+
+  getMovieCast(id: any) {
+    this.service.getMovieCast(id).subscribe((result) => {
+      console.log(result, 'movieCast#');
+      this.getMovieCastResult = result.cast;
+    });
+  }
 }
--- showtime/src/app/service/movie-api-service.service.ts
+++ showtime/src/app/service/movie-api-service.service.ts
@@ -31,4 +31,23 @@ export class MovieApiServiceService {
       `${this.baseurl}/search/movie?api_key=${this.apikey}&query=${data.movieName}`
     );
   }
+  // getmoviedatails
+  getMovieDetails(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseurl}/movie/${data}?api_key=${this.apikey}`
+    );
+  }
+
+  // getMovieVideo
+  getMovieVideo(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseurl}/movie/${data}/videos?api_key=${this.apikey}`
+    );
+  }
+  // getMovieCast
+  getMovieCast(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseurl}/movie/${data}/credits?api_key=${this.apikey}`
+    );
+  }
 }

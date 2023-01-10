9d98c9c900f0fbee28852a4bedafee8eb8b11036
Diogo Guerra <diogoguerra25@gmail.com>
2023-01-10 11:03:42 +0000

    Change navbar title

--- showtime/src/app/app.component.ts
+++ showtime/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, HostListener } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'showtime';
+  title = 'DGFlix';
   navbg: any;
 
   @HostListener('document:scroll') scrollover() {
